118ffe5c812eb6652f7cfe561d1e12ea66f102d5
heena <heena7sept@gmail.com>
2022-02-25 14:46:24 -0600

    Jose is commiting this change

--- test/src/test/Hello.java
+++ test/src/test/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		System.out.println("Hi, Heena is making this change");
 		System.out.println("I am in cs3443");
 		System.out.println("Hi, I am Alice, I am making this change");
+		System.out.println("I am Jose");
 		System.out.println(" I like to eat icecream");
 
 	}
